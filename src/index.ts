cabfb3ed14bcb282f65b0f0513f0ff7891773931
JuanMarchetto <juanpatriciomarchetto@gmail.com>
2022-06-21 22:44:48 -0300

    creating some basic functions

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,31 @@
-const message = 'Hello, world!';
-console.log(message);
+const axios = require('axios').default;
+require('dotenv').config()
+
+const BASE_URL = "https://rooftop-career-switch.herokuapp.com/";
+
+const API = async (
+  method: 'get' | 'post',
+  url: string,
+  params?: {
+    blocks?: Array<string>,
+    encoded?: string
+  }
+) => {
+  return await axios[method](
+    `${BASE_URL}${url}?token=${process.env.TOKEN}`,
+    params
+  )
+  .then(({ data }:{data:{message?:string, data:Array<string>}}) => data)
+};
+
+const getBlocks = () => API('get', 'blocks');
+
+const getCheck = (firstBlock: string, secondBlock: string) =>
+  API('post', 'check', { blocks: [firstBlock, secondBlock] });
+
+(async () => {
+  const {data: blocks} = await getBlocks()
+  console.log(blocks);
+  const {message: check} = await getCheck(blocks[0], blocks[1])
+  console.log(check)
+})()
